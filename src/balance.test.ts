063300d25e9f8069f5403bbff791a966085ece78
Julien Calixte <julienc@bam.tech>
2022-06-05 21:48:10 +0200

    Being stuck when adding an expense

--- src/balance.test.ts
+++ src/balance.test.ts
@@ -18,4 +18,52 @@ describe('balance', () => {
       }
     })
   })
+
+  it('equalizes multiple expenses from one account between 2 accounts', () => {
+    const balance = new Balance({
+      account1: [
+        {
+          cost: 12,
+          receivers: ['account1', 'account2']
+        },
+        {
+          cost: 10,
+          receivers: ['account1', 'account2']
+        }
+      ]
+    })
+
+    expect(balance.calculate()).toEqual({
+      account2: {
+        account1: 11
+      }
+    })
+  })
+
+  it('equalizes multiple expenses from the 2 accounts', () => {
+    const balance = new Balance({
+      account1: [
+        {
+          cost: 12,
+          receivers: ['account1', 'account2']
+        },
+        {
+          cost: 10,
+          receivers: ['account1', 'account2']
+        }
+      ],
+      account2: [
+        {
+          cost: 10,
+          receivers: ['account1', 'account2']
+        }
+      ]
+    })
+
+    expect(balance.calculate()).toEqual({
+      account2: {
+        account1: 6
+      }
+    })
+  })
 })
--- src/balance.ts
+++ src/balance.ts
@@ -1,11 +1,17 @@
 import { AccountExpenses } from '@/types/expense'
 import { Refund } from '@/types/refund'
 
+/**
+ * It is not about calibrating expense by expense, but simply equalizing
+ * at the end.
+ * Splitting the ones who added more to the group and the ones who received
+ * the most is finally a good idea.
+ */
 export class Balance {
   constructor(private readonly accountExpenses: AccountExpenses) {}
 
   public calculate(): Refund {
-    let refund: Refund = {}
+    let accountRefunds: Refund = {}
 
     for (const funder in this.accountExpenses) {
       for (const expense of this.accountExpenses[funder]) {
@@ -14,15 +20,23 @@ export class Balance {
             continue
           }
 
-          refund = {
-            [receiver]: {
-              [funder]: expense.cost / expense.receivers.length
-            }
+          let refund = expense.cost / expense.receivers.length
+
+          if (accountRefunds?.[funder]?.[receiver]) {
+            accountRefunds[funder][receiver] = Math.min(
+              0,
+              accountRefunds[funder][receiver] - refund
+            )
+          }
+
+          accountRefunds[receiver] = {
+            ...accountRefunds[receiver],
+            [funder]: (accountRefunds[receiver]?.[funder] ?? 0) + refund
           }
         }
       }
     }
 
-    return refund
+    return accountRefunds
   }
 }
